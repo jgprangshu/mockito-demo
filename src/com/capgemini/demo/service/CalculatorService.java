5635766c443b5e121ffe3dc129862402e2142ff7
Gogoi <prangshu-jyoti.gogoi@capgemini.com>
2019-03-27 18:20:54 +0530

    added mockito demo project

---
+++ src/com/capgemini/demo/service/CalculatorService.java
@@ -0,0 +1,12 @@
+package com.capgemini.demo.service;
+
+public interface CalculatorService {
+	
+	public int addition(int number1 , int number2);
+	public int substraction(int number1 , int number2);
+	public int multiplication(int number1 , int number2);
+	public int division(int number1 , int number2);
+	public int factorial(int number);
+	public int square(int number);
+
+}
